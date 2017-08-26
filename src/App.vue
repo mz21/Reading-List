3d4752d898f9a835bd0abc95178e6e9767e25633
Michael Zhong <mzhong21@Michaels-iMac.fios-router.home>
2017-08-26 00:04:39 -0400

    initial setup, setup sass/jade, included bourbon/bitters

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="jade">
+#app
+  img(src="./assets/logo.png")
+  router-view
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="sass">
+@import 'globals/base'
+</style>
